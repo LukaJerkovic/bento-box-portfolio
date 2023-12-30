bddc194c8c3e1f4a1328e9b1c5ca15e98eb34e8b
LukaJerkovic <lujerkovic@gmail.com>
2023-12-30 13:30:24 +0100

    Added navigation and home page.

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,108 @@
-import Image from 'next/image'
+'use client';
+
+import { motion, useAnimation } from 'framer-motion';
+import { useEffect } from 'react';
+import Navbar from '@/components/Navbar';
+import { Button } from '@/components/ui/button';
+import Image from 'next/image';
+import { Linkedin, Github } from 'lucide-react';
+import Link from 'next/link';
 
 export default function Home() {
+  const controlsLeft = useAnimation();
+  const controlsRight = useAnimation();
+
+  const animateSections = async () => {
+    await controlsLeft.start({
+      opacity: [0, 1],
+      x: [150, 0],
+      transition: { duration: 1 },
+    });
+
+    await controlsRight.start({
+      opacity: 1,
+      x: 0,
+      transition: { duration: 1 },
+    });
+  };
+
+  useEffect(() => {
+    animateSections();
+  }, []);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
+    <main className="flex flex-col min-h-screen bg-[#0C1B33]">
+      <motion.nav>
+        <Navbar />
+      </motion.nav>
+      <section
+        id="home"
+        className="flex flex-col sm:flex-row flex-grow p-8 m-0 mt-0"
+      >
+        <motion.div
+          className="sm:w-1/2 lg:w-full bg-[#F5FBEF] m-2 rounded-3xl p-4 lg:p-8 border-4 border-[#AD343E]"
+          animate={controlsLeft}
+        >
+          <div className="lg:w-full sm:w-1/2 bg-[#F5FBEF] flex flex-col items-start p-4">
             <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+              src="/profile.jpeg"
+              width={200}
+              height={200}
+              className="rounded-full mb-2"
+              alt="profile"
             />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+            <h1 className="mt-8 text-2xl lg:text-5xl font-semibold">
+              Developer passionate about building user-friendly websites.
+            </h1>
+            <p className="mt-4 text-xl lg:text-3xl leading-relaxed">
+              Focused on continuous learning and skill improvement in web
+              development. Passionate about engaging in collaborative projects
+              and seeking mentorship opportunities to broaden knowledge and
+              expertise in the ever-evolving landscape of web development.
+            </p>
+          </div>
+        </motion.div>
+        <motion.div
+          className="w-full sm:w-1/2 flex flex-col"
+          initial={{ opacity: 0, x: 100 }}
+          animate={controlsRight}
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+          <div className="bg-[#F5FBEF] flex-grow m-2 rounded-3xl p-4 lg:p-8 border-4 border-[#AD343E] flex flex-col items-center justify-center">
+            <h2 className="text-2xl lg:text-3xl text-center lg:mb-12">
+              Explore my projects and don&apos;t hesitate to get in touch if
+              you&apos;re interested in collaborating.
+            </h2>
+            <Button className="w-64 lg:h-16 rounded-3xl m-4 p-4 text-lg lg:hover:scale-125 bg-[#AD343E] lg:hover:bg-[#AD343E]">
+              Send a Message
+            </Button>
+          </div>
+          <div className="bg-[#F5FBEF] flex-grow m-2 rounded-3xl p-4 lg:p-8 border-4 border-[#AD343E] flex flex-col items-center justify-center">
+            <p className="text-lg lg:text-2xl leading-relaxed">
+              or find me on social media:
+            </p>
+            <ul className="flex space-x-4">
+              <li>
+                <Link
+                  href="https://www.linkedin.com/in/lujerkovic/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <Linkedin className="w-8 h-8 lg:w-12 lg:h-12" />
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="https://github.com/LukaJerkovic"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <Github className="w-8 h-8 lg:w-12 lg:h-12" />
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </motion.div>
+      </section>
     </main>
-  )
+  );
 }
---
+++ components/Navbar.tsx
@@ -0,0 +1,84 @@
+'use client';
+
+import { useState } from 'react';
+
+import { ChevronDownCircle, ChevronUpCircle } from 'lucide-react';
+import Link from 'next/link';
+
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <nav>
+      <div className="flex items-center justify-between p-6 mx-8 my-6 bg-[#F5FBEF] rounded-3xl border-4 border-[#AD343E]">
+        <div className="flex items-center">
+          <Link
+            href="/"
+            className="text-[#0C1B33] pr-8 hover:first-letter:text-red-800 hover:scale-125 hover:pl-2 lg:text-lg"
+          >
+            .lukajerkovic
+          </Link>
+        </div>
+        <div className="hidden md:flex items-center space-x-8">
+          <Link
+            href="/"
+            className="text-[#0C1B33] pr-8 hover:first-letter:text-red-800 lg:text-lg hover:scale-125"
+          >
+            .home
+          </Link>
+          <Link
+            href="/projects"
+            className="text-[#0C1B33] pr-8 hover:first-letter:text-red-800 lg:text-lg hover:scale-125"
+          >
+            .projects
+          </Link>
+          <Link
+            href="/contact"
+            className="text-[#0C1B33] pr-8 hover:first-letter:text-red-800 lg:text-lg  hover:scale-125"
+          >
+            .contact
+          </Link>
+        </div>
+        <div className="md:hidden">
+          <button onClick={toggleMenu} className="text-[#0C1B33]">
+            {isOpen ? (
+              <ChevronUpCircle className="text-red-800" size={24} />
+            ) : (
+              <ChevronDownCircle className="text-[#0C1B33]" size={24} />
+            )}
+          </button>
+        </div>
+      </div>
+      {isOpen && (
+        <div className="md:hidden absolute w-[85%] p-6 bg-[#F5FBEF] m-8 mt-0 rounded-3xl border-4 border-[#AD343E]">
+          <div className="flex flex-col space-y-4">
+            <Link
+              href="/"
+              className="text-[#0C1B33] pr-6 hover:first-letter:text-red-800 hover:scale-125 hover:pl-8"
+            >
+              .home
+            </Link>
+            <Link
+              href="/"
+              className="text-[#0C1B33] pr-6 hover:first-letter:text-red-800 hover:scale-125 hover:pl-8"
+            >
+              .projects
+            </Link>
+            <Link
+              href="/"
+              className="text-[#0C1B33] pr-6 hover:first-letter:text-red-800 hover:scale-125 hover:pl-8"
+            >
+              .contact
+            </Link>
+          </div>
+        </div>
+      )}
+    </nav>
+  );
+};
+
+export default Navbar;
